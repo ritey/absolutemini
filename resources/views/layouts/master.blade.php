58d3e060cb45d4a5375039aabfa948852802aeea
David Wright <dave@ritey.com>
2015-12-29 17:03:52 +0000

    Link update

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -55,7 +55,7 @@
 
     <!-- Begin Cookie Consent plugin by Silktide - http://silktide.com/cookieconsent -->
     <script type="text/javascript">
-        window.cookieconsent_options = {"message":"This website uses cookies to ensure you get the best experience on our website","dismiss":"Got it!","learnMore":"More info","link":"http://doblotto.com/privacy","theme":"light-bottom"};
+        window.cookieconsent_options = {"message":"This website uses cookies to ensure you get the best experience on our website","dismiss":"Got it!","learnMore":"More info","link":"http://absolutemini.com/privacy","theme":"light-bottom"};
     </script>
 
     <script async type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/1.0.9/cookieconsent.min.js"></script>
