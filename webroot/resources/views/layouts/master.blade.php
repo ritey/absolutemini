47692b4406c70a67cc2f17b1d1316278e07a79b6
David Wright <dave@ritey.com>
2022-10-14 15:43:06 +0100

    Update GA tag

--- webroot/resources/views/layouts/master.blade.php
+++ webroot/resources/views/layouts/master.blade.php
@@ -22,16 +22,6 @@
 
         @yield('head')
 
-        <script>
-        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-        ga('create', 'UA-418381-1', 'auto');
-        ga('send', 'pageview');
-
-        </script>
         <style>
             @charset "UTF-8";
 
@@ -11135,6 +11125,15 @@ footer a:first-child {
   }
 }</style>
     </head>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-L6DWWZKS0L"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-L6DWWZKS0L');
+    </script>
     <body>
 
         @include('partials.menu')
